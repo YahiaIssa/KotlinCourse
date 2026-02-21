0338f774e2458664654dc17cb943e10280bfbb1c
yahia <yramadan200105@gmail.com>
2026-02-21 20:54:54 +0300

    Function in kotlin calc the circle ,triangle,square

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,27 @@
 package org.kotlion.unlimted
 
 
-
 fun main() {
-    //Casting data types
-    val finalExamScore: String="4.7ghjgh"
-    val result = finalExamScore + 1
-    val y=(finalExamScore.toDoubleOrNull()?:0.0) + 1.0
-    println(y)
-    println(result)
 
+
+   val result= calculatcirclearea(5.2)
+    println("CArea $result cm")
+
+    calculetSequarearea(10.0)
+    calculetTringle(10.5,5.6)
+}
+var factor = 60
+fun calculatcirclearea(redius: Double): Double {
+    val pi: Double = 3.14
+    return redius * redius * pi
+}
+fun calculetSequarearea( Side : Double ) {
+
+    val SArea = Side * Side
+    println("SArea : $SArea cm2")
+}
+fun  calculetTringle(base : Double ,height : Double)
+{
+val Tarea=0.5*base*height
+    println("Tarea : $Tarea cm")
 }
