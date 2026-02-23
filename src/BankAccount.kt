605bed347a66503449aecbeba04bcbbff371f6db
yahia <yramadan200105@gmail.com>
2026-02-24 01:48:09 +0300

    Bank example using Encapsulation

--- src/BankAccount.kt
+++ src/BankAccount.kt
@@ -1,5 +1,23 @@
+package org.kotlion.unlimted
 import  java.util.UUID
-class BankAccount(val fullname: String,) {
-val accountId: UUID = UUID.randomUUID()
-    var balance= 0
+class BankAccount(
+    val fullname: String) {
+
+    val accountId: UUID = UUID.randomUUID()
+    private var balance = 0
+
+    fun deposit(amount: Int) {
+        balance += amount
+    }
+    fun withdraw(amount: Int): Boolean {
+        if (balance >= amount) {
+            balance -= amount
+            return true
+        }else {
+            return false
+        }
+    }
+    fun getBalance(): Int {
+        return balance
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,11 @@
 package org.kotlion.unlimted
 
 fun main() {
-    val firstStudent: Student = Student("Yahia", "yahia@kotlin.com",22, true)
-    val scondeStudent: Student = Student("Hamzeh", "Hamzeh@kotlin.com",23 ,false)
-    val therdStudent: Student = Student("Waleed", "waleed@kotlin.com", 24,false)
-    val forthStudent: Student = Student("Hashiam", "Hashiam@kotlin.com",25, false)
-
-
-    firstStudent.shareApost()
-    scondeStudent.shareApost()
-
+val myBankAccount = BankAccount(fullname = "yahia issa ramadan")
+    myBankAccount.deposit(10000)
+    val iswithdrawSuccess = myBankAccount.withdraw(5000)
+    println("is withdraw success: $iswithdrawSuccess \n the blance after the proccess is : \n ${myBankAccount.getBalance()}")
+    val iswithdrawSuccess2 = myBankAccount.withdraw(5000)
+    println("is withdraw success: $iswithdrawSuccess \n the blance after the proccess is : \n ${myBankAccount.getBalance()}")
 }
 
