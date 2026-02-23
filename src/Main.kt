7a77eb63c04d2cfda62b16e6e931da8cb710dfc1
yahia <yramadan200105@gmail.com>
2026-02-23 12:21:18 +0300

    Calc fizz buzz in different way

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,56 @@
 package org.kotlion.unlimted
 
 fun main() {
-println(FizzBuzz(20))
+    check(
+        name = "when enter 1 return",
+        result = calculateSingleNumberFizzBuzz(1),
+        correctResult = "1"
+    )
+
+    check(
+        name = "when enter 3 return Fizz",
+        result = calculateSingleNumberFizzBuzz(3),
+        correctResult = "fizz"
+    )
+
+    check(
+        name = "when enter 5 return Buzz",
+        result = calculateSingleNumberFizzBuzz(5),
+        correctResult = "buzz"
+    )
+
+    check(
+        name = "when enter 15",
+        result = calculateSingleNumberFizzBuzz(15),
+        correctResult = "fizzbuzz"
+    )
+
+    // لو بدك تطبع القائمة كاملة
+    // println(fizzBuzz(20))
 }
-fun FizzBuzz(number:Int): List<String> {
-    val result : MutableList<String> = mutableListOf()
-    for (currentnumber in 1..number) {
-       val solution= calcsingleFizzBuzz(currentnumber)
-   result.add(solution)
+
+fun fizzBuzz(number: Int): List<String> {
+    val result = mutableListOf<String>()
+    for (i in 1..number) {
+        result.add(calculateSingleNumberFizzBuzz(i))
     }
- return result
+    return result
 }
-fun calcsingleFizzBuzz(number:Int): String {
-   return when {
-        number % 3 == 0 && number % 5 == 0 -> "Fizz"
-       number %3 ==0-> "Fizz"
-       number % 5 == 0 -> "Buzz"
-       else -> number.toString()
+
+fun calculateSingleNumberFizzBuzz(number: Int): String {
+    return ""
+//    when {
+//        number % 3 == 0 && number % 5 == 0 -> "fizzbuzz"
+//        number % 3 == 0 -> "fizz"
+//        number % 5 == 0 -> "buzz"
+//        else -> number.toString()
+//    }
+}
+
+fun check(name: String, result: String, correctResult: String) {
+    if (result == correctResult) {
+        println("Success - $name")
+    } else {
+        println("Failed - $name | expected: $correctResult, got: $result")
     }
-
-
 }
-
-
-
