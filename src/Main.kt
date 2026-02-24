7e64d4d6f267d09b9986b91210278efa05e33bfb
yahia <yramadan200105@gmail.com>
2026-02-25 01:22:11 +0300

    Extensions in kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -5,40 +5,14 @@ import javax.print.attribute.standard.JobStateReason
 
 
 fun main() {
-    val homeScreenstate: Screenstate= Loding()
-
-when(homeScreenstate){
-    is Error -> {homeScreenstate.reason}
-    is Loding -> { TODO() }
-    is Success -> { homeScreenstate.post }
+   val welcomeMassage: String="hello world"
+  println(welcomeMassage.something)
+    String.space
 }
-
+fun String.getWordsCount(): Int {
+    return this.trim().split(String.space).size
 }
-sealed class Screenstate
-class Success(val post: List<Post>): Screenstate()
-class Loding(): Screenstate()
-class Error(val reason: String): Screenstate()
-
-//val day: WeekDays=WeekDays.FRIDAY
-//    println(day.daynameinArabic)
-//    when(day) {
-//        WeekDays.SATURDAY -> TODO()
-//        WeekDays.SUNDAY -> TODO()
-//        WeekDays.MONDAY -> TODO()
-//        WeekDays.TUESDAY -> TODO()
-//        WeekDays.WEDNESDAY -> TODO()
-//        WeekDays.THURSDAY -> TODO()
-//        WeekDays.FRIDAY -> TODO()
-//    }
-//    }
-//enum class WeekDays(val daynameinArabic : String) {
-//    SATURDAY("السبت"),
-//    SUNDAY("الاحد"),
-//    MONDAY("الاثنين"),
-//    TUESDAY("الثلاثاء"),
-//    WEDNESDAY("الاربعاء"),
-//    THURSDAY("الخميس"),
-//    FRIDAY("الجمعة"),
-//
-
-
+ val String.something: Int
+     get()=this.length/2
+val String.Companion.space: String
+get() = ""
