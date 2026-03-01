2914c0acc59573b06d77439f67de9c00972f4721
yahia <yramadan200105@gmail.com>
2026-03-01 23:48:36 +0300

    Collection Transformation in kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,31 @@
 package org.kotlin.taxiapp.vehicles
 
-import org.kotlion.unlimted.trip.BasicTrip
-import org.kotlion.unlimted.trip.FoodDecorator
-import org.kotlion.unlimted.trip.MusicDecorator
-import org.kotlion.unlimted.trip.Trip
-import org.kotlion.unlimted.trip.TripDecorator
-import org.kotlion.unlimted.trip.WifiDecorator
-
+import jdk.dynalink.Operation
+import java.io.DataInput
 
 fun main() {
-    val input=1
+    val todayTemperaterPerHours: List<Int> =
+        listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+    val happyNumber: List<String> = listOf("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"
+        ,"Ten ","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"
+        ,"Ten")
 
-    println("factorial ($input) = ${factorial(input)}")
-}
-fun factorial(input: Int): Long {
-    return if (input==1) 1 else input * factorial(input-1)
+    val foodCategory: List<Set<String>> = listOf(
+        setOf("Apple", "Banana", "Orange", "Pear"),
+        setOf("Carrot", "Broccli", "Spinach"),
+        setOf("Chicken", "Beef", "Fish")
+    )
+
+    println(
+        todayTemperaterPerHours.zip(happyNumber)
+    )
+//    println(
+//        foodCategory.flatten()
+//    )
+//    println()
+//    todayTemperaterPerHours.map (::finedDublicate )
+//}
+//fun finedDublicate(input: Int): Int{
+//    return input*2
 }
 
