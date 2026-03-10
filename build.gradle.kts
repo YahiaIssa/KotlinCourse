3ad5f36b97576a67918f248b7ac23bdc7bfc82e8
yahia <yramadan200105@gmail.com>
2026-03-10 07:23:08 +0300

    Api

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    kotlin("plugin.serialization") version "1.8.20"
 }
 
 group = "org.example"
@@ -10,8 +11,20 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("io.arrow-kt:arrow-core:1.2.4")
+    implementation("io.insert-koin:koin-core:4.0.2")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testImplementation("com.google.truth:truth:1.4.2")
+    testImplementation("io.mockk:mockk:1.13.16")
+
+
+    implementation("io.ktor:ktor-client-core:2.3.13")
+    implementation("io.ktor:ktor-client-cio:2.3.13")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 }
 
 kotlin {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,28 @@
 package org.example
 
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.launch
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.runBlocking
 
-fun main() {
-    startSearchListener()
-    runBlocking {
-        delay(2000)
-        search("A")
-        delay(100)
-        search("Ap")
-        delay(60)
-        search("App")
-        delay(120)
-        search("Appl")
-        delay(600)
-        search("Apple")
 
-        delay(10000)
+
+fun main() {
+    runBlocking {
+
+        getWaetherBylocation()
     }
 
-}
-val searchFlow: MutableStateFlow<String> = MutableStateFlow(" ")
-suspend fun search(keyword:String){
-searchFlow.emit(keyword)
-}
-@OptIn(DelicateCoroutinesApi::class, FlowPreview::class)
-fun startSearchListener(){
-    GlobalScope.launch {
-        searchFlow.filter (String::isNotBlank)
-            .debounce(500)
-            .collect (::query)
-        } }
 
-
-fun query(input: String){
-    println("searching about : $input")
+}
+suspend fun printGoogleContent(){
+    val client= HttpClient(CIO)
+   val response= client.get("https://www.google.com")
+    println(response.bodyAsText())
+}
+suspend fun getWaetherBylocation(){
+    val client= HttpClient(CIO)
+    val response= client.get("")
+    println(response.bodyAsText())
 }
